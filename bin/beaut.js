66ab66e4c117091d1a4e728c9569f747510b6837
Daniel Stockman <daniels@zillow.com>
2011-12-14 19:41:32 -0800

    Initial commit.
    Adds js-beautify.js as of 20111214, linted and made easier to read.
    Initial stab at npm config and executable.

---
+++ bin/beaut.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+var nopt = require('nopt'),
+
+    options = {
+        indent_size: Number,
+        indent_char: String,
+        brace_style: ['collapse', 'expand', 'end-expand'],
+        keep_array_indentation: Boolean,
+        preserve_newlines: Boolean,
+        preserve_max_newlines: Number,
+        jslint_happy: Boolean,
+        usage: Boolean
+    },
+    shorthand = {
+        i: ['--indent_size'],
+        b: ['--brace_style', 'expand'],
+        a: ['--keep_array_indentation'],
+        n: ['--preserve_newlines'],
+        p: ['--jslint_happy'],
+
+        h: ['--usage'],
+        help: ['--usage'],
+        '?': ['--usage']
+    },
+    config = nopt(options, shorthand),
+
+    beautify;
+
+if (config.usage || !config.cooked) {
+    // user passed -h or no args
+    console.log([
+        "Usage: beaut [options] [file || URL || STDIN]",
+        "",
+        "Reads from standard input if no file or URL is specified.",
+        "",
+        "Options:",
+        "-i NUM\tIndent size (1 for TAB)",
+        "-b\tPut braces on own line (Allman / ANSI style)",
+        "-a\tIndent arrays",
+        "-n\tPreserve newlines",
+        "-p\tJSLint-pedantic mode, currently only adds space between \"function ()\"",
+        "",
+        "-h\tPrint this help",
+        "",
+        "Examples:",
+        "  beaut -i 2 example.js",
+        "  beaut -i 1 http://www.example.org/example.js",
+        "  beaut < example.js"
+    ].join( "\n" ));
+
+    process.exit();
+}
+
+beautify = require('../lib/beautify').js_beautify;
